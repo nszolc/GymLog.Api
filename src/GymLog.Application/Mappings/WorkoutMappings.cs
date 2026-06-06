cf3c01d7f40f45f1b73b92382f19928d643e0891
Natalia <nataliaszolc.kontakt@gmail.com>
2026-06-06 20:51:37 +0200

    feat: add validators and mappings for workout entity

---
+++ src/GymLog.Application/Mappings/WorkoutMappings.cs
@@ -0,0 +1,32 @@
+﻿using GymLog.Application.DTOs.Workouts;
+using GymLog.Domain.Entities;
+
+namespace GymLog.Application.Mappings;
+
+public static class WorkoutMappings
+{
+    public static WorkoutDto ToDto(this Workout workout)
+    {
+        return new WorkoutDto
+        {
+            Id = workout.Id,
+            Date = workout.Date,
+            Notes = workout.Notes
+        };
+    }
+
+    public static Workout ToEntity(this CreateWorkoutDto dto)
+    {
+        return new Workout
+        {
+            Date = dto.Date,
+            Notes = dto.Notes
+        };
+    }
+
+    public static void UpdateEntity(this UpdateWorkoutDto dto, Workout workout)
+    {
+        workout.Date = dto.Date;
+        workout.Notes = dto.Notes;
+    }
+}
---
+++ src/GymLog.Application/Validators/Workouts/CreateWorkoutDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using GymLog.Application.DTOs.Workouts;
+using FluentValidation;
+namespace GymLog.Application.Validators.Workouts;
+
+public class CreateWorkoutDtoValidator : AbstractValidator<CreateWorkoutDto>
+{
+    public CreateWorkoutDtoValidator()
+    {
+        RuleFor(x => x.Date).NotEmpty();
+        RuleFor(x => x.Notes).MaximumLength(500);
+    }
+    
+}
